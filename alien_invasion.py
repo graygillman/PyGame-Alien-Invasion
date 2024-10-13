d8b08ae1c6a6967dd28d7fb1b82f565873c95e7a
graygillam <graygillman@gmail.com>
2024-10-13 13:04:39 -0500

    Adjusting the Ship’s Speed

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,6 +17,10 @@ class AlienInvasion:
 
         self.ship = Ship(self)
 
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+
 
 
 
@@ -36,9 +40,14 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
